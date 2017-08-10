d51e7b79fc7537a451338e9492984905f4dfa2dc
gnardini <gonzalo.nardini.dev@gmail.com>
2017-08-10 16:32:57 -0300

    Adds image and image reader

---
+++ image_reader.py
@@ -0,0 +1,26 @@
+import io
+import cv2
+import numpy as np
+
+sizes = {
+    'GIRL.RAW': (389, 164),
+    'BARCO.RAW': (290, 207),
+    'LENA.RAW': (256, 256),
+    'GIRL2.RAW': (256, 256),
+    'FRACTAL.RAW': (200, 200),
+}
+
+def read(file_path):
+    file_name = file_path.split('/')[-1]
+    if file_name in sizes:
+        return read_raw(file_path, sizes[file_name][0], sizes[file_name][1])
+    else:
+        return cv2.imread(file_path)
+
+def read_raw(file, width, height):
+    image = np.zeros((height, width, 1), np.uint8)
+    with io.open(file, 'r', encoding='mac_roman') as f:
+        text = f.read()
+        for k in range(len(text)):
+            image[k // width][k % width] = ord(text[k])
+    return image
