ebee8d8bf8cdd618fe4dc3f9c4ccd2ae7a286966
gnardini <gonzalo.nardini.dev@gmail.com>
2017-08-10 17:49:33 -0300

    Adds function for mouse click binding to visualizer

--- visualizer.py
+++ visualizer.py
@@ -4,7 +4,10 @@ import numpy as np
 def stack_images(images):
     return np.hstack(images)
 
-def show_image(image):
-    cv2.imshow('ATI', image)
+def show_image(image, window='ATI'):
+    cv2.imshow(window, image)
     cv2.waitKey(0)
 
+def bindMouseCallbacks(fx, window='ATI'):
+    cv2.namedWindow(window)
+    cv2.setMouseCallback(window, fx)
