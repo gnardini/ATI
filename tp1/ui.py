67f2fd6def03026b7cd7f7fbd1d9ff4f04f76e81
gnardini <gonzalo.nardini.dev@gmail.com>
2017-08-20 19:32:35 -0300

    Rearrangement of UI

--- tp1/ui.py
+++ tp1/ui.py
@@ -26,71 +26,76 @@ def select_image(targetPanel):
         return np.zeros(0)
     return img_array
 
-def assign_image_a():
-    global original_image_a
-    global modify_image_a
-    original_image_a = select_image(originalPanelA)
-
-def assign_image_b():
-    global original_image_b
-    global modify_image_b
-    original_image_b = select_image(originalPanelB)
+def assign_image(key):
+    images[key] = select_image(panels[key])
 
 #TODO: fix this function
-def save_image():
+def save_image(key):
     path = filedialog.asksaveasfilename()
     if path:
-        image_io.save_image(modify_image_a, path)
+        image_io.save_image(images[key], path)
 
-def put_into(panel, img):
-    modify_image_a = img #TODO: this should be change for both
+def put_into(key, img):
+    images[key] = img
     img = to_tk_image(img)
-    panel.configure(image=img)
-    panel.image = img
+    panels[key].configure(image=img)
+    panels[key].image = img
 
 root = Tk()
 
-original_image_a = None
-original_image_b = None
-modify_image_a = None
-modify_image_b = None
+panels = {
+    'original-up': None,
+    'original-down': None,
+    'result-up': None,
+    'result-down': None
+}
+images = {
+    'original-up': None,
+    'original-down': None,
+    'result-up': None,
+    'result-down': None
+}
 
 # Imagenes y botones para agregarlas
 base_row = 2
-originalPanelA = Label(root)
-originalPanelA.grid(row=base_row, column=0, columnspan=3)
-resultPanelA = Label(root)
-resultPanelA.grid(row=base_row, column=3, columnspan=3)
-btnA = Button(root, text="Elegir imagen", command=assign_image_a)
+panels['original-up'] = Label(root)
+panels['original-up'].grid(row=base_row, column=0, columnspan=3)
+panels['result-up'] = Label(root)
+panels['result-up'].grid(row=base_row, column=3, columnspan=3)
+btnA = Button(root, text="Elegir imagen", command=lambda: assign_image('original-up'))
 btnA.grid(row=base_row+1, column=0)
-originalPanelB = Label(root)
-originalPanelB.grid(row=base_row+2, column=0, columnspan=3)
-btnB = Button(root, text="Elegir imagen", command=assign_image_b)
+btnA = Button(root, text="Mover abajo", command=lambda: put_into('original-down', images['original-up']))
+btnA.grid(row=base_row+1, column=1)
+btnA = Button(root, text="Guardar imagen", command=lambda: save_image('result-up'))
+btnA.grid(row=base_row+1, column=3)
+btnA = Button(root, text="Mover a izquierda", command=lambda: put_into('original-up', images['result-up']))
+btnA.grid(row=base_row+1, column=4)
+panels['original-down'] = Label(root)
+panels['original-down'].grid(row=base_row+2, column=0, columnspan=3)
+btnB = Button(root, text="Elegir imagen", command=lambda: assign_image('original-down'))
 btnB.grid(row=base_row+3, column=0)
-resultPanelB = Label(root)
-resultPanelB.grid(row=base_row+2, column=3, columnspan=3)
+panels['result-down'] = Label(root)
+panels['result-down'].grid(row=base_row+2, column=3, columnspan=3)
 
 # Botones de transformacion
-btn = Button(root, text='Sumar', command=lambda: put_into(resultPanelA, ops.add_images(original_image_a, original_image_b)))
+btn = Button(root, text='Sumar', command=lambda: put_into('result-up', ops.add_images(images['original-up'], images['original-down'])))
 btn.grid(row=0, column=0)
-btn = Button(root, text='Restar', command=lambda: put_into(resultPanelA, ops.subtract_images(original_image_a, original_image_b)))
+btn = Button(root, text='Restar', command=lambda: put_into('result-up', ops.subtract_images(images['original-up'], images['original-down'])))
 btn.grid(row=0, column=1)
-btn = Button(root, text='Multiplicar', command=lambda: put_into(resultPanelA, ops.multiply_images(original_image_a, original_image_b)))
+btn = Button(root, text='Multiplicar', command=lambda: put_into('result-up', ops.multiply_images(images['original-up'], images['original-down'])))
+btn.grid(row=1, column=0)
+btn = Button(root, text='Negativo', command=lambda: put_into('result-up', ops.negative(images['original-up'])))
+btn.grid(row=1, column=1)
+btn = Button(root, text='Histograma', command=lambda: ops.grayscale_histogram(images['original-up']))
 btn.grid(row=0, column=2)
-btn = Button(root, text='Histograma', command=lambda: ops.grayscale_histogram(original_image_a))
-btn.grid(row=0, column=3)
-btn = Button(root, text='Negativo', command=lambda: put_into(resultPanelA, ops.negative(original_image_a)))
+btn = Button(root, text='Ecualización', command=lambda: put_into('result-up', ops.equalize(images['original-up'])))
+btn.grid(row=1, column=2)
+btn = Button(root, text='Contraste', command=lambda: put_into('result-up', ops.increase_contrast(images['original-up'])))
 btn.grid(row=0, column=4)
-btn = Button(root, text='Contraste', command=lambda: put_into(resultPanelA, ops.increase_contrast(original_image_a)))
-btn.grid(row=0, column=5)
 scale = Scale(root, from_=0, to=255, orient=HORIZONTAL)
 scale.set(128)
-scale.grid(row=0, column=6)
-btn = Button(root, text='Umbralización', command=lambda: put_into(resultPanelA, ops.apply_threshold(original_image_a, scale.get())))
-btn.grid(row=1, column=6)
-btn = Button(root, text='Ecualización', command=lambda: put_into(resultPanelA, ops.equalize(original_image_a)))
-btn.grid(row=0, column=7)
-btn = Button(root, text='Guardar', command=lambda: save_image())
-btn.grid(row=0, column=8)
+scale.grid(row=0, column=3)
+btn = Button(root, text='Umbralización', command=lambda: put_into('result-up', ops.apply_threshold(images['original-up'], scale.get())))
+btn.grid(row=1, column=3)
 
 root.mainloop()
